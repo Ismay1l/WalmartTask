fa0944a3ec4b66b982be8de0b5c9b7e7d5de337a
Ismay1l <ismayilovismayil1226@gmail.com>
2024-04-16 19:07:17 -0400

    Initial Commit

---
+++ Walmart_Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Walmart_Task
+//
+//  Created by Ismayil Ismayilov on 4/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
